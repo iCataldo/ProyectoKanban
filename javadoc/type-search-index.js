44200c1fd0e430ad5c7cd2aec87b0ca414c69b6e
Ignacio Cataldo <146041107+iCataldo@users.noreply.github.com>
2023-12-19 16:53:18 -0300

    Add javadoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"CentralGrid"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"NameGrid"},{"p":"<Unnamed>","l":"Tasks"},{"p":"<Unnamed>","l":"Window"}]
